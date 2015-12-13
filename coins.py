a0f1314f7b865590852dc057e921bd1efce74615
Saurabh Singhal <singhal.saurabh.mnnit@gmail.com>
2015-12-13 17:15:22 +0530

    Added comments

--- coins.py
+++ coins.py
@@ -1,15 +1,17 @@
 import sys
-import pdb 
 
 def calMaxAmt( coins ):
     if coins <= 4:
         return coins
     if coins in dp.keys():
         return dp[ coins ]
+    # Memoization
     dp[ coins ] = max( coins, calMaxAmt( coins / 2 ) + calMaxAmt( coins / 3 ) + calMaxAmt( coins / 4 ) )
     return dp[ coins ]
 
+# Input until EOF
 for line in sys.stdin:
+    # Reset the dictionary for every testcase
     dp = {}
     maxAmt = calMaxAmt( int( line ) )
     print int( maxAmt )
